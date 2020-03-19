7c051e1bd2ceb91f2107e09c23c6ec72746c2065
antoniocastilloz <47866299+antoniocastilloz@users.noreply.github.com>
2020-03-19 20:07:58 -0300

    Exibindo dados da busca no SearchModal

--- src/components/Application/Search/ModalSearch.vue
+++ src/components/Application/Search/ModalSearch.vue
@@ -3,8 +3,15 @@
     <v-card>
       <v-card-title class="headline px-4 py-3 cyan white--text">Adicionar Livro<v-spacer></v-spacer><v-icon @click="closeModalSearch" color="white">mdi-close</v-icon></v-card-title>
 
-      <v-card-text class="pb-2 pt-4">{{ModalSearchUI}}</v-card-text>
-
+      <v-card-text class="px-3 pb-1 pt-3">
+        <img align="left" :src="ModalSearchUI.volumeInfo.imageLinks.thumbnail">
+        Titulo: {{ModalSearchUI.volumeInfo.title}}<br>
+        Descrição: {{ModalSearchUI.volumeInfo.description}}<br>
+        Autores: {{ModalSearchUI.volumeInfo.authors}}<br>
+        Número de páginas: {{ModalSearchUI.volumeInfo.pageCount}}<br>
+        Data de publicação: {{ModalSearchUI.volumeInfo.publishedDate}}<br>
+        Mais informações: {{ModalSearchUI.volumeInfo.infoLink}}<br>
+        </v-card-text>
       <v-card-actions style="display:flex;">
         <v-btn style="flex-grow:2;" color="cyan white--text" raised @click="dialog = false">Já li</v-btn>
         <v-btn style="flex-grow:1;" color="cyan white--text" raised @click="dialog = false">Estou Lendo</v-btn>
